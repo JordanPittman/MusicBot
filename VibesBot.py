70ca17f1152b21c57f54563366e90cc4f64482d3
Chadwick Weiler <Chadwickweiler@u.boisestate.edu>
2024-03-09 17:28:10 -0700

    Closes #15 - Basic functionality of "Hello" working as intended.

--- VibesBot.py
+++ VibesBot.py
@@ -1,26 +1,30 @@
-#Bare Bones Bot implementation
+# Bare Bones Bot implementation
 
-#imports
+# imports
 from Secrets import DISCORD_TOKEN, YOUTUBE_API_KEY
-import discord #will need more from this application
 from discord.ext import commands
-import youtube_dl #will need this when we get to youtube implementation
+import discord  # will need more from this application
+# import youtube_dl
+# will need above when we get to youtube implementation
 
 CHANNEL_ID = 1216170695986384907
 
-#this makes it so the bot commands are recognized as starting with '!'
+# this makes it so the bot commands are recognized as starting with '!'
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
-#event handler that outputs when the bot is online
+
+# event handler that outputs when the bot is online
 @bot.event
-async def on_ready(): #overrides on_ready in discord program 
-    print(f'{bot.user.name} is ready to create a mood')
+async def on_ready():  # overrides on_ready in discord program
+    print('VibesBot is ready to create a mood')
     channel = bot.get_channel(CHANNEL_ID)
-    await channel.send(f'{bot.user.name} is ready to create a mood')
+    await channel.send('VibesBot is ready to create a mood')
+
 
 @bot.command()
-async def intro(ctx):
-    await ctx.send("Hiya!")
+async def hello(ctx):
+    await ctx.send("Hello")
 
-#gets discord token from untracked Secrets file for security
+
+# gets discord token from untracked Secrets file for security
 bot.run(DISCORD_TOKEN)

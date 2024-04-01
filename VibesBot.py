39d480871d6e9d629b83bfe061cc47fb50260614
abbiesarmento <97471248+abbiesarmento@users.noreply.github.com>
2024-04-01 15:36:08 -0600

    Task11 (#21)

--- VibesBot.py
+++ VibesBot.py
@@ -2,6 +2,7 @@
 
 # imports
 from Secrets import DISCORD_TOKEN, YOUTUBE_API_KEY
+from googleapiclient.discovery import build
 from discord.ext import commands
 import discord  # will need more from this application
 # import youtube_dl
@@ -12,13 +13,17 @@ CHANNEL_ID = 1216170695986384907
 # this makes it so the bot commands are recognized as starting with '!'
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
+# Initialize YouTube API client
+youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
 
 # event handler that outputs when the bot is online
 @bot.event
 async def on_ready():  # overrides on_ready in discord program
     print('VibesBot is ready to create a mood')
+    print('VibesBot is ready to play music')
     channel = bot.get_channel(CHANNEL_ID)
     await channel.send('VibesBot is ready to create a mood')
+    await channel.send('VibesBot is ready to play music')
 
 
 @bot.command()

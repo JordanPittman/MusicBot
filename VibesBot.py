9c3b9c59c3504e47e19aec88c509b388ac4d4fa8
abbiesarmento <97471248+abbiesarmento@users.noreply.github.com>
2024-04-02 10:33:58 -0600

    Closes #23 (#24)

--- VibesBot.py
+++ VibesBot.py
@@ -9,6 +9,7 @@ import discord  # will need more from this application
 # will need above when we get to youtube implementation
 
 CHANNEL_ID = 1216170695986384907
+MUSIC_CHANNEL_ID = 1224486311449329696
 
 # this makes it so the bot commands are recognized as starting with '!'
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
@@ -29,6 +30,16 @@ async def on_ready():  # overrides on_ready in discord program
 @bot.command()
 async def hello(ctx):
     await ctx.send("Hello")
+
+@bot.command()
+async def join(ctx):
+    v_channel = bot.get_channel(MUSIC_CHANNEL_ID)
+    if ctx.author.voice:
+        v_channel = ctx.author.voice.channel
+        await v_channel.connect()
+        await ctx.send("Joined the voice channel")
+    else:
+        await ctx.send("You are not in a voice channel")
 
 
 # gets discord token from untracked Secrets file for security

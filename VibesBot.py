ad619044d52a27a1a83b8ce468152a37c48229b7
Jordan Pittman <97776912+JordanPittman@users.noreply.github.com>
2024-04-02 16:58:59 -0600

    Task12 (#27)
    Closing Unnecessary 
    Co-authored-by: ChadwickWeiler <77372775+ChadwickWeiler@users.noreply.github.com>

--- VibesBot.py
+++ VibesBot.py
@@ -47,6 +47,55 @@ async def join(ctx):
         await ctx.send("You are not in a voice channel")
 
 
+# Command for playing music and functionality for the queue/playlist
+@bot.command()
+async def play(ctx, url: str):
+    global is_playing
+
+    # Check if the user is in a voice channel
+    if ctx.author.voice and ctx.author.voice.channel:
+        voice_channel = ctx.author.voice.channel
+
+        # Connect to the voice channel if the bot is not already connected
+        if voice_channel not in [vc.channel for vc in bot.voice_clients]:
+            voice_client = await voice_channel.connect()
+        else:
+            voice_client = discord.utils.get(bot.voice_clients, channel=voice_channel)
+
+        # Fetch the YouTube video
+        video = YouTube(url)
+
+        # Get the audio stream URL
+        audio_url = video.streams.filter(only_audio=True).first().url
+
+        # Get the title of the song
+        title = video.title
+
+        # Add the song to the playlist
+        playlist.append((title, audio_url))
+
+        # If no song is currently playing, play the first song in the playlist
+        if not is_playing:
+            await play_next(ctx, voice_client)
+
+        # Send a message to Discord saying the song is being added to the queue
+        await ctx.send(f"Added to the queue: {title}")
+
+    else:
+        await ctx.send("You need to be in a voice channel to play music.")
+
+async def play_next(ctx, voice_client):
+    global is_playing
+
+    if not voice_client or not voice_client.is_connected():
+        is_playing = False
+        return
+    # Attempt to try and keep the music playing if it gets interrupted (may need some tweaking)
+    if playlist:
+        is_playing = True
+        title, audio_url = playlist.pop(0)
+
+
 # Command for playing music
 @bot.command()
 async def play(ctx, url: str):
@@ -66,6 +115,23 @@ async def play(ctx, url: str):
             'options': '-vn',
         }
         source = discord.FFmpegPCMAudio(audio_url, **ffmpeg_options)
+
+        transformed_source = discord.PCMVolumeTransformer(source)
+
+        def after_playing(error):
+            fut = asyncio.run_coroutine_threadsafe(play_next(ctx, voice_client), bot.loop)
+            try:
+                fut.result()
+            except:
+                # Handle errors if needed
+                pass
+
+        voice_client.play(transformed_source, after=after_playing)
+        await ctx.send(f"Now playing: {title}")
+    else:
+        is_playing = False
+        await ctx.send("There are no more songs in the queue.")
+
         voice_client.play(source)
         await ctx.send(f"Now playing: {video.title}")
     else:

18cf3030a838d9cb641dfa14be0096134510167d
Jordan Pittman <jordanpittman@u.boisestate.edu>
2024-05-16 19:05:32 -0600

    Update setup.sh to handle tar file format and use wget

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@
 mkdir -p /app/bin
 
 # Download precompiled ffmpeg binary
-curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz -o ffmpeg-release-64bit-static.tar.xz
-tar -xJf ffmpeg-release-64bit-static.tar.xz
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz -O ffmpeg-release-64bit-static.tar.xz
+tar -xf ffmpeg-release-64bit-static.tar.xz
 
 # Find the extracted directory name dynamically and copy the binaries
 extracted_dir=$(tar -tf ffmpeg-release-64bit-static.tar.xz | head -1 | cut -f1 -d"/")
